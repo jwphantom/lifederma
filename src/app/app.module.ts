54f8c63c21fc2b173063413936b822c045f41a6c
jwphantom <44881349+jwphantom@users.noreply.github.com>
2021-01-03 23:48:39 +0100

    first commit erp lifederma CRUD order + crud product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,34 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import{ GlobalConstants } from './common/global-constants';
+import { DatePicker } from '@ionic-native/date-picker/ngx';
+import { DatePipe } from '@angular/common';
+
+
+
+const config: SocketIoConfig = { url: "http://localhost:3001", options: {} };
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserModule, 
+    HttpClientModule,
+    IonicModule.forRoot(),
+    SocketIoModule.forRoot(config), 
+    AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
+    DatePipe,
+    DatePicker,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
