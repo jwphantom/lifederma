eaaf8eff038d058a6d0feedda4c5a2322754994f
jwphantom <44881349+jwphantom@users.noreply.github.com>
2022-03-19 00:30:02 +0100

    change global constant apiURL

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -1,7 +1,7 @@
 export class GlobalConstants {
-    //public static baseUrl = 'https://server-endoftheage.herokuapp.com/api';
-    public static apiURL = 'http://localhost:3001/api';
-    public static version: string = "0.4.1";
+    public static apiURL = 'https://server-scare.herokuapp.com/api';
+    //public static apiURL = 'http://localhost:3001/api';
+    public static version: string = "1.0.0";
     public static appname: string = "LifeDerma";
     public static localisation: string = "Yaoundé, Cameroun";
     public static devise: string = "XAF";
